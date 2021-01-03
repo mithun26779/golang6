b62e188f391308e6897b077988e90f4226a68e8b
mithun shashidhara <sashidm@wz.hasbro.com>
2021-01-03 22:21:44 +0530

    F

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mithun26779/golang6
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+)
+
+
+
+
+func main() {
+
+    fmt.Println("go3 main")
+}
+
+
+func init() {
+    fmt.Println("go3 init 2")
+}
+
+func init() {
+    fmt.Println("go3 init 1")
+}
+
